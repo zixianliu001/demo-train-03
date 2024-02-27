6769b7ac4343f2b7f66a8a651cc1116444285bed
lzx <3514524762@qq.com>
2024-02-27 17:15:33 +0800

    Initial Commit

---
+++ demo-train-03/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo-train-03
+//
+//  Created by 魏宏 on 2024/2/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demo-train-03/demo_train_03App.swift
@@ -0,0 +1,17 @@
+//
+//  demo_train_03App.swift
+//  demo-train-03
+//
+//  Created by 魏宏 on 2024/2/27.
+//
+
+import SwiftUI
+
+@main
+struct demo_train_03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
